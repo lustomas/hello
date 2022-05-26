d8926feb0b87c921328a526e17a680332489cee4
Tomasz Luszczak <luszczaktomasz@gmail.com>
2022-05-26 22:38:23 +0200

    Update files

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return render_template('parent.html')
+
 @app.route("/mypage/me")
 def me():
-    return render_template("me.html")
+    return render_template('me.html')
 
 @app.route('/mypage/contact', methods=['GET', 'POST'])
 def contact():
    if request.method == 'GET':
-       return render_template("contact.html")
+       return render_template('contact.html')
    elif request.method == 'POST':
        print(request.contact)
        return redirect("/mypage/me")
